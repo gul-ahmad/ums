8844aec2e630dbe47d51135078501ce3dd2a8a54
Gul Muhammad <gulmuhammad57@yahoo.com>
2025-06-11 22:35:30 +0500

    working on authentication

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,19 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
+Route::get("/user", function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum');
+})->middleware("auth:sanctum");
+
+Route::group(["prefix" => "auth"], function () {
+    Route::post("login", [AuthController::class, "login"]);
+    Route::post("register", [AuthController::class, "register"]);
+
+    Route::group(["middleware" => "auth:sanctum"], function () {
+        Route::get("logout", [AuthController::class, "logout"]);
+        Route::get("user", [AuthController::class, "user"]);
+    });
+});
