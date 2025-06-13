49402d7e3c47f3e59cf512a8f707c7df6ea1eb4a
Gul Muhammad <gulmuhammad57@yahoo.com>
2025-06-13 18:38:39 +0500

    working on listing of roles

--- resources/js/navigation/vertical/index.js
+++ resources/js/navigation/vertical/index.js
@@ -9,4 +9,20 @@ export default [
     to: { name: 'second-page' },
     icon: { icon: 'tabler-file' },
   },
+  {
+    title: 'User',
+    icon: { icon: 'tabler-user' },
+    children: [
+      { title: 'List', to: 'user-list' },
+      { title: 'View', to: { name: 'user-view-id', params: { id: 21 } } },
+    ],
+  },
+  {
+    title: 'Roles & Permissions',
+    icon: { icon: 'tabler-lock' },
+    children: [
+      { title: 'Roles', to: 'roles' },
+      // { title: 'Permissions', to: 'permissions' },
+    ],
+  },
 ]
